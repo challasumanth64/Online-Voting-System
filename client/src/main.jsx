81776a180c3751155e18e234434760da5ef51d76
challasumanth64 <challasumanth64@users.noreply.github.com>
2025-09-05 18:24:12 +0530

    Commit remaining changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
-        import { createRoot } from 'react-dom/client'
-        import App from './App'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/design-system.css'
 
-        const root = createRoot(document.getElementById('root'))
-        root.render(<App />)
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
